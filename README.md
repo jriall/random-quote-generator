7157997e225809cb2a1ed6d378657aa5de2e2df0
JamesRiall <jamesrariall@gmail.com>
2017-09-01 07:43:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Random-Quote-Generator
+A Random Quote Generator built as part of the Free Code Camp Front End Curriculum. First utilisation of RESTful web APIs - pulling random quotes from an API onto the page and allowing the user to tweet the author and quote as well as generate a new random quote.
+
+Built in April 2017.
---
+++ quotes.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  // set url variable
+  var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+
+  //define function to get new quote
+  var newQuote = function(data) {
+    var text = data.quoteText;
+    var author = data.quoteAuthor;
+    $(".quote-text").html("\"" + text + "\"");
+    var twitterQuote = "https://twitter.com/intent/tweet?text=" + "\"" + text + "\" " + author + " via @JamesRiall";
+    if (author === '') {
+      author = 'Unknown';
+    }
+    $(".quote-author").text(author);
+    $(".twitter-share-button").attr("href", twitterQuote);
+  };
+
+//call new quote on page load
+  $.getJSON(url, newQuote);
+
+//get new quote on button click
+  $("#quote-button").click(function() {
+    $.getJSON(url, newQuote);
+  });
+});
